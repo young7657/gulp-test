13274dac5200b73dc45089f9d74085322ab8c157
youngliu <youngliu129@126.com>
2015-09-22 11:47:35 +0800

    使用gulp编译sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 // 首先引入对应的模块
 var gulp = require('gulp');
+// sass编译
+var sass = require('gulp-sass');
 
 /**
  * 编写第一个task
@@ -7,4 +9,11 @@ var gulp = require('gulp');
  */
 gulp.task('hello', function() {
 	console.log('hellp gulp');
+});
+
+// 接下来使用gulp编译scss文件
+gulp.task('sass', function() {
+	return gulp.src('app/scss/styles.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('app/css'))
 });
